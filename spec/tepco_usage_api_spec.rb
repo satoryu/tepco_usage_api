c9b322573ab2d7929bb124edf65583720bb6d51f
Tatsuya Sato <satoryu.1981@gmail.com>
2011-03-27 02:11:14 +0900

    Refactor to emphasize the target method is #latest

--- spec/tepco_usage_api_spec.rb
+++ spec/tepco_usage_api_spec.rb
@@ -2,6 +2,8 @@ require File.expand_path(File.join(
           File.dirname(__FILE__), 'spec_helper'))
 
 describe TepcoUsage do
-  subject{TepcoUsage}
-  its(:latest) { should_not be_nil } 
+  describe "#latest" do
+    subject{TepcoUsage}
+    its(:latest) { should_not be_nil } 
+  end
 end
