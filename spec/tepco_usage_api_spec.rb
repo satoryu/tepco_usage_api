b87142ecda9c2363e1c9a7913fe59e9d4ec43d48
Tatsuya Sato <satoryu.1981@gmail.com>
2011-03-27 02:15:36 +0900

    Make a stub of TepcoUsage#request

--- spec/tepco_usage_api_spec.rb
+++ spec/tepco_usage_api_spec.rb
@@ -3,7 +3,28 @@ require File.expand_path(File.join(
 
 describe TepcoUsage do
   describe "#latest" do
+    before do
+      TepcoUsage.stub!(:request).with('/latest.json').and_return do
+        <<-JSON
+           {
+             "saving": false, 
+              "hour": 0, 
+              "capacity_updated": "2011-03-26 09:30:00", 
+              "month": 3, 
+              "usage_updated": "2011-03-26 16:30:54", 
+              "entryfor": "2011-03-26 15:00:00", 
+              "capacity_peak_period": 18, 
+              "year": 2011, 
+              "usage": 2904, 
+              "capacity": 3700, 
+              "day": 27
+           }
+        JSON
+      end
+    end
+
     subject{TepcoUsage}
     its(:latest) { should_not be_nil } 
+    
   end
 end
