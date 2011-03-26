6e96f1aabe222a6049241168dbd9c7855ac44645
Tatsuya Sato <satoryu.1981@gmail.com>
2011-03-27 02:22:45 +0900

    Modify spec to make stub of TepcoUsage#request return mock

--- spec/tepco_usage_api_spec.rb
+++ spec/tepco_usage_api_spec.rb
@@ -5,19 +5,19 @@ describe TepcoUsage do
   describe "#latest" do
     before do
       TepcoUsage.stub!(:request).with('/latest.json').and_return do
-        <<-JSON
+        mock = mock(Object.new, :body => <<-JSON)
            {
              "saving": false, 
-              "hour": 0, 
-              "capacity_updated": "2011-03-26 09:30:00", 
-              "month": 3, 
-              "usage_updated": "2011-03-26 16:30:54", 
-              "entryfor": "2011-03-26 15:00:00", 
-              "capacity_peak_period": 18, 
-              "year": 2011, 
-              "usage": 2904, 
-              "capacity": 3700, 
-              "day": 27
+             "hour": 0, 
+             "capacity_updated": "2011-03-26 09:30:00", 
+             "month": 3, 
+             "usage_updated": "2011-03-26 16:30:54", 
+             "entryfor": "2011-03-26 15:00:00", 
+             "capacity_peak_period": 18, 
+             "year": 2011, 
+             "usage": 2904, 
+             "capacity": 3700, 
+             "day": 27
            }
         JSON
       end
@@ -25,6 +25,9 @@ describe TepcoUsage do
 
     subject{TepcoUsage}
     its(:latest) { should_not be_nil } 
+    it do
+      TepcoUsage.latest['saving'].should be_false
+    end
     
   end
 end
